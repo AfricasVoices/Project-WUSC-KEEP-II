2de1114c8d5c433be3f0f20b906d3551c4f6756b
Isaack Mwenda <mwendaisaac@gmail.com>
2019-11-26 16:07:28 +0300

    git add lib

---
+++ src/lib/__init__.py
@@ -0,0 +1,4 @@
+from .code_schemes import CodeSchemes
+from .consent_utils import ConsentUtils
+from .icr_tools import ICRTools
+from .message_filters import MessageFilters
---
+++ src/lib/code_schemes.py
@@ -0,0 +1,37 @@
+import json
+
+from core_data_modules.data_models import CodeScheme
+
+
+def _open_scheme(filename):
+    with open(f"code_schemes/{filename}", "r") as f:
+        firebase_map = json.load(f)
+        return CodeScheme.from_firebase_map(firebase_map)
+
+
+class CodeSchemes(object):
+    DADAAB_S01E01_REASONS = _open_scheme("dadaab_s01e01_reasons.json")
+    DADAAB_S01E02_REASONS = _open_scheme("dadaab_s01e02_reasons.json")
+    DADAAB_S01E03_REASONS = _open_scheme("dadaab_s01e03_reasons.json")
+    DADAAB_S01E04_REASONS = _open_scheme("dadaab_s01e04_reasons.json")
+    DADAAB_S01E05_REASONS = _open_scheme("dadaab_s01e05_reasons.json")
+    DADAAB_S01E06_REASONS = _open_scheme("dadaab_s01e06_reasons.json")
+    DADAAB_S01E07_REASONS = _open_scheme("dadaab_s01e07_reasons.json")
+
+    KAKUMA_S01E01_REASONS = _open_scheme("kakuma_s01e01_reasons.json")
+    KAKUMA_S01E02_REASONS = _open_scheme("kakuma_s01e02_reasons.json")
+    KAKUMA_S01E03_REASONS = _open_scheme("kakuma_s01e03_reasons.json")
+    KAKUMA_S01E04_REASONS = _open_scheme("kakuma_s01e04_reasons.json")
+    KAKUMA_S01E05_REASONS = _open_scheme("kakuma_s01e05_reasons.json")
+    KAKUMA_S01E06_REASONS = _open_scheme("kakuma_s01e06_reasons.json")
+    KAKUMA_S01E07_REASONS = _open_scheme("kakuma_s01e07_reasons.json")
+
+    GENDER = _open_scheme("gender.json")
+    NATIONALITY = _open_scheme("nationality.json")
+    AGE = _open_scheme("age.json")
+    DADAAB_HH_LANGUAGE = _open_scheme("dadaab_hh_language.json") # We have different language & location schemes for the two camps
+    DADAAB_LOCATION = _open_scheme("dadaab_location.json")
+    KAKUMA_HH_LANGUAGE = _open_scheme("kakuma_hh_language.json")
+    KAKUMA_LOCATION = _open_scheme("kakuma_location.json")
+
+    WS_CORRECT_DATASET = _open_scheme("ws_correct_dataset.json")
