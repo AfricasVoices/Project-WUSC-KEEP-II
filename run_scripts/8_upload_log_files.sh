62979a5cd69f3e856804f15e01b23c814eae5e4b
Isaack Mwenda <mwendaisaac@gmail.com>
2021-04-07 12:55:50 +0300

    fix upload log files arguments

--- run_scripts/8_upload_log_files.sh
+++ run_scripts/8_upload_log_files.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ $# -ne 6 ]]; then
-    echo "Usage: ./8_upload_log_files.sh <user> <google-cloud-credentials-file-path> <pipeline-configuration-file-path> <run-id> <memory-profile-dir> <data-archive-dir>"
+if [[ $# -ne 5 ]]; then
+    echo "Usage: ./8_upload_log_files.sh <user> <google-cloud-credentials-file-path> <pipeline-configuration-file-path> <memory-profile-dir> <data-archive-dir>"
     echo "Uploads the pipeline's log files"
     exit
 fi
@@ -11,10 +11,9 @@ fi
 USER=$1
 GOOGLE_CLOUD_CREDENTIALS_FILE_PATH=$2
 PIPELINE_CONFIGURATION_FILE_PATH=$3
-RUN_ID=$4
-MEMORY_PROFILE_DIR=$5
-DATA_ARCHIVE_DIR=$6
+MEMORY_PROFILE_DIR=$4
+DATA_ARCHIVE_DIR=$5
 
 cd ..
 pipenv run python upload_log_files.py "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION_FILE_PATH" \
-    "$RUN_ID" "$MEMORY_PROFILE_DIR" "$DATA_ARCHIVE_DIR"
+    "$MEMORY_PROFILE_DIR" "$DATA_ARCHIVE_DIR"
